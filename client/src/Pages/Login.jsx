58fb39897e3d8db1fa4dd17ff7796b4bcc4c6c2b
JUDHI3134 <judhistirbehera241@gmail.com>
2024-03-25 15:29:12 +0530

    create and add OAth component continue with google

--- client/src/Pages/Login.jsx
+++ client/src/Pages/Login.jsx
@@ -6,6 +6,7 @@ import {
   signInFailure,
 } from "../redux/user/userSlice";
 import { useDispatch, useSelector } from "react-redux";
+import OAuth from "../Components/OAuth";
 
 const Login = () => {
   const [formData, setFormData] = useState({});
@@ -65,6 +66,7 @@ const Login = () => {
         >
           {loading ? "Loading..." : "Sign In"}
         </button>
+        <OAuth/>
       </form>
       <div className="flex gap-2 mt-5">
         <p>Don't Have an Account?</p>
