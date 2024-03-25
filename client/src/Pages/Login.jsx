22b82d6e497085315caebf0e2bb16d9ab48d9401
JUDHI3134 <judhistirbehera241@gmail.com>
2024-03-25 08:37:49 +0530

    create signup page functionality

--- client/src/Pages/Login.jsx
+++ client/src/Pages/Login.jsx
@@ -1,11 +1,76 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link, useNavigate} from "react-router-dom";
 
 const Login = () => {
-  return (
-    <div>
-      Login
-    </div>
-  )
-}
+  const [formData, setFormData] = useState({});
+  const [error,setError] = useState(false)
+  const [loading,setLoading] = useState(false)
 
-export default Login
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+
+    
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true)
+      setError(false)
+      const res = await fetch("/api/auth/signin", {
+        method:'POST',
+        headers:{
+          'Content-Type':'application/json',
+        },
+        body:JSON.stringify(formData)
+      });
+      const data = await res.json();
+      setLoading(false)
+      if(data.success == false){
+        setError(true)
+        return;
+      }
+      navigate("/")
+    } catch (error) {
+      setLoading(false)
+      setError(true)
+    }
+  };
+
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl font-semibold text-center my-7">Sign Up</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+       
+        <input
+          type="email"
+          id="email"
+          placeholder="Email"
+          className="bg-slate-100 p-3 rounded-lg"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          id="password"
+          placeholder="Password"
+          className="bg-slate-100 p-3 rounded-lg"
+          onChange={handleChange}
+        />
+        <button disabled={loading} className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
+          {loading ? 'Loading...' : "Sign In"}
+        </button>
+      </form>
+      <div className="flex gap-2 mt-5">
+        <p>Don't Have an Account?</p>
+        <Link to="/sign-up">
+          <span className="text-blue-500">Sign Up</span>
+        </Link>
+      </div>
+      <p className="text-red-700 mt-5">{error && 'Something Went Wrong...'}</p>
+    </div>
+  );
+};
+
+export default Login;
