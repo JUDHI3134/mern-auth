3554668bca96ac8f12397742dfaba3a2516d009d
JUDHI3134 <judhistirbehera241@gmail.com>
2024-03-25 10:41:20 +0530

    add redux toolkit

--- client/src/Pages/Login.jsx
+++ client/src/Pages/Login.jsx
@@ -1,41 +1,43 @@
 import React, { useState } from "react";
-import { Link, useNavigate} from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from "../redux/user/userSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 const Login = () => {
   const [formData, setFormData] = useState({});
-  const [error,setError] = useState(false)
-  const [loading,setLoading] = useState(false)
+  const { loading, error } = useSelector((state) => state.user);
 
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
-
-    
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      setLoading(true)
-      setError(false)
+      dispatch(signInStart());
       const res = await fetch("/api/auth/signin", {
-        method:'POST',
-        headers:{
-          'Content-Type':'application/json',
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
         },
-        body:JSON.stringify(formData)
+        body: JSON.stringify(formData),
       });
       const data = await res.json();
-      setLoading(false)
-      if(data.success == false){
-        setError(true)
+      if (data.success == false) {
+        dispatch(signInFailure(data));
         return;
       }
-      navigate("/")
+      dispatch(signInSuccess(data));
+      navigate("/");
     } catch (error) {
-      setLoading(false)
-      setError(true)
+      dispatch(signInFailure(error));
     }
   };
 
@@ -43,7 +45,6 @@ const Login = () => {
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Sign Up</h1>
       <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-       
         <input
           type="email"
           id="email"
@@ -58,8 +59,11 @@ const Login = () => {
           className="bg-slate-100 p-3 rounded-lg"
           onChange={handleChange}
         />
-        <button disabled={loading} className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
-          {loading ? 'Loading...' : "Sign In"}
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          {loading ? "Loading..." : "Sign In"}
         </button>
       </form>
       <div className="flex gap-2 mt-5">
@@ -68,7 +72,7 @@ const Login = () => {
           <span className="text-blue-500">Sign Up</span>
         </Link>
       </div>
-      <p className="text-red-700 mt-5">{error && 'Something Went Wrong...'}</p>
+      <p className="text-red-700 mt-5">{error ? error.message || "Something Went Wrong..." : ""}</p>
     </div>
   );
 };
